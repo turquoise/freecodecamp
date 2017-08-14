429e6fd0698d50cd4eb26df33389d279e2754979
turquoise <monica.gosschalk@btinternet.com>
2017-08-14 15:14:38 +0100

    added quote api

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from './quote.model';
+import { QuoteService } from './quote.service';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
+  providers: [QuoteService]
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  quote: Quote;
+
+  randomQuote: any;
+
+
+  constructor(private quoteService: QuoteService) {
+    quoteService.generateRandomQuotes(1000, (quote) => {
+      this.quote = quote;
+    });
+  }
 
   ngOnInit() {
+    this.getRandomQuote();
   }
+
+  getRandomQuote() {
+    this.quoteService.getQuotesApi()
+      .subscribe( result => {
+        console.log('result ', result);
+        this.randomQuote = result;
+      });
+  }
+
+  onClick() {
+    this.getRandomQuote();
+  }
+
+
 
 }
