728a3af038f7d3a90ccf649298caf82ec7871380
turquoise <monica.gosschalk@btinternet.com>
2017-08-19 15:44:42 +0100

    added openweather and geoid apis

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './weather.service';
+import { Subscription } from 'rxjs/Subscription';
 import * as $ from 'jquery';
 
 @Component({
@@ -10,39 +11,92 @@ import * as $ from 'jquery';
 })
 export class WeatherComponent implements OnInit {
 
-  cityWeather;
+  subscription: Subscription;
+
+  celsius: boolean = false;
+  weatherData;
+  geoip;
+  country;
+  city;
+  lat;
+  long;
+  icon;
+  f;
+  c;
+
+  iconUrl;
+
   // from kelvin to celcius subtract 273
-  backgroundImages = [];
-  ip = [];
-  location = {}
+  backgroundImages = [
+    { "299 thunder": "http://i3.somersetlive.co.uk/incoming/article75204.ece/ALTERNATES/s1200/Thunder.jpg"}, // up to 299 it is thunder storm
+    { "499 drizzle": "https://c1.staticflickr.com/7/6191/6113430622_370016e5a9_z.jpg"}, // up to 499 it is drizzle between 299 and 499
+    { "599 rain": "https://sites.psu.edu/siowfa16/files/2016/10/rain-generic_650x400_71457950721-1043ddi.jpg"}, // from 499 to 599 it is rain.
+    { "699 snow": "http://media.phillyvoice.com/media/images/01282017_light_snow_on_roads_i.2e16d0ba.fill-1200x630-c0.jpg"}, // up to 699 it is snow.
+    { "799 fog": ""}, // up to 799 it is fog.
+    { "800 clear": ""}, //just 800 it is clear sky.
+    { "801 cloudy": ""} // anything over 801 is cloudy.
+  ];
+  // https://openweathermap.org/weather-conditions
+  icons;
 
-  constructor(private weatherService: WeatherService) { }
-
-  ngOnInit() {
-    this.getWeather();
-    //this.getGeoIp(this.mylocation);
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.location = position.coords;
-        console.log('position.coords ', position.coords);
-      })
-    }
+  constructor(private weatherService: WeatherService) {
 
   }
 
-  // convertTemp() {
-  //
-  // }
+  ngOnInit() {
 
-
-
-  getWeather() {
-    this.weatherService.getWeather()
-      .subscribe( result => {
-        console.log('London weather', result);
-        this.cityWeather = result;
+    this.getgeoip();
+    this.subscription = this.weatherService.weatherDataChanged.subscribe(
+      () => {
+        this.weatherData = this.weatherService.getWeatherData();
+        this.icon = this.weatherData.weather[0].icon;
+        this.iconUrl = 'http://openweathermap.org/img/w/' + this.icon + '.png';
+        console.log('this.icon ', this.icon);
+        console.log('this.iconUrl ', this.iconUrl);
+        console.log('this.weatherData from component ', this.weatherData);
       })
 
+    }
+
+
+    onToggle() {
+      this.celsius = !this.celsius;
+      console.log('this.celsius ', this.celsius);
+    }
+
+
+  displayTemp(temp) {
+
+    if (this.celsius) {
+      this.c = Math.round( (temp-32) * (5/9)) + ' C';
+      this.celsius = true;
+      console.log('this.c ', this.c);
+      return this.c;
+    } else {
+      this.f = Math.round(temp) + ' F';
+      this.celsius = false;
+      console.log('this.f ', this.f);
+      return this.f;
+    }
+  }
+
+
+
+
+  getgeoip() {
+    this.weatherService.getGeoip()
+      .subscribe( res => {
+        this.geoip = res;
+        console.log('this.geoip ', this.geoip);
+        this.city = res.city;
+        this.country = res.country.name;
+        this.lat = res.location.latitude;
+        this.long = res.location.longitude;
+        console.log('this.city ', this.city);
+        console.log('this.country ', this.country);
+        console.log('this.lat ', this.lat);
+        console.log('this.long ', this.long);
+      })
   }
 
 }
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Jsonp, URLSearchParams, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class WeatherService {
 
-  private APIKEY = '46f0d79940ce443ee80d2a76da8bcdb8';
 
+  weatherDataChanged = new Subject<any[]>();
+  weatherData;
   constructor(
     private http: Http,
     private jsonp: Jsonp
@@ -15,16 +18,43 @@ export class WeatherService {
   // https://openweathermap.org/api
   // https://gist.github.com/P1xt/6aca515e3a4c02756adf06e0ebe1c941
 
+  getWeatherData() {
+    console.log('this.weatherData from the service ', this.weatherData);
+    return this.weatherData;
+  }
 
-  getWeather() {
 
-    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${this.APIKEY}`)
-      .map(res => {
-        const result = res.json();
-        return result;
-      });
+  getWeather(lat , lon) {
+    let APIKEY = '46f0d79940ce443ee80d2a76da8bcdb8';
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=${APIKEY}`)
+      .map(response => response.json())
+      //   const result = res.json();
+        //console.log('result ', result);
+        //return result;
+      //})
+      .subscribe( result => {
+        console.log('this.weatherData', result);
+        this.weatherData = result;
+        this.weatherDataChanged.next();
+        return this.weatherData;
+      })
 
   }
 
-  
+
+  getGeoip() {
+    return this.http.get('http://geoip.nekudo.com/api/')
+      .map( result => {
+        const resultIp = result.json();
+        const lat = resultIp.location.latitude;
+        const lon = resultIp.location.longitude;
+        this.getWeather(lat, lon);
+        console.log('resultIp ', resultIp);
+        console.log('lat, lon ', lat, lon);
+        return resultIp;
+
+      })
+
+  }
+
 }
