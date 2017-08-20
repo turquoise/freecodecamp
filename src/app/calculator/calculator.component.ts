cc8e0c2f9759b5e06432cd4da158fb9f4d7ba5b6
turquoise <monica.gosschalk@btinternet.com>
2017-08-20 15:05:47 +0100

    completed calculator

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  box = '';
+  result;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  toScreen(x) {
+    this.box += x;
+    if (x === 'c') {
+      this.box = '';
+    }
+  }
+
+  backspace() {
+    let num = this.box;
+    let len = num.length - 1;
+    let newNum = num.substring(0,len);
+    this.box = newNum;
+  }
+
+  power() {
+    let x;
+    x = this.box;
+    x = x * x;
+    this.box = x;
+  }
+
+  answer() {
+    // eval(string)
+    this.result = eval(this.box);
+    console.log('this.result ', this.result);
+    this.box = this.result;
+  }
+
 
 }
