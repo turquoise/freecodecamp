2da20c5d20232755fe04e4c1d1da925c3d70f592
turquoise <monica.gosschalk@btinternet.com>
2017-08-22 18:03:29 +0100

    added simple random to tictactoe

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -8,99 +8,158 @@ import * as _ from 'lodash';
 })
 export class TicTacToeComponent implements OnInit {
 
+  // gameboard = [
+  //   "sq1", "sq2", "sq3",
+  //   "sq4", "sq5", "sq6",
+  //   "sq7", "sq8", "sq9"
+  // ];
+
   gameboard = [
-    "sq1", "sq2", "sq3",
-    "sq4", "sq5", "sq6",
-    "sq7", "sq8", "sq9"
+    "", "", "",
+    "", "", "",
+    "", "", ""
   ];
+
+  gameboard2 = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
   game = [];
   square;
+  sqx = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  sqo = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  squareSelected;
+  hidegameboard = false;
   player = 1;
   random = Math.floor(Math.random() * 9) + 1;
   rand = Math.floor(Math.random() * 9);
   tag = ".sq" + this.rand.toString();
   gamewon = false;
+  reload = false;
+  gamewonPlayer = false;
+  gamewonComputer = false;
 
   constructor() { }
 
   // https://www.youtube.com/watch?v=7YySrAQF_Jg
 
-  // define variables
-  // when I click on a square
-    // check if it has been selected.
-    // add class x based on player
-      // check if I did win.
-    // toggle player
-    // have computer select random square.
-      // did the computer win?
-
   ngOnInit() {
-    this.startGame();
-    this.randomSquare(this.rand);
-    this.didIWin('x');
+    //this.startGame();
+    //this.randomSquare(this.rand);
 
   }
 
-  startGame() {
-
+  onReload() {
+    this.gamewon = false;
   }
 
   onClick(square) {
     console.log('clicked on ', square);
     let num = parseInt(square);
-    if ( _.includes(this.gameboard[num], 'x') || _.includes(this.gameboard[num], 'o') ) {
-      console.log('sorry the square is taken');
+    if ( _.includes(this.gameboard2[num], 'x') || _.includes(this.gameboard2[num], 'o') ) {
+      console.log('This square has been selected.');
     } else {
-      console.log('you have selected ', this.gameboard[num]);
+      if (this.player === 1) {
+        //this.gameboard[num] = 'sq' + num.toString() + 'x';
+        this.gameboard2[num] = 'x';
+        //console.log('you have selected ', this.gameboard2[num]);
+        console.log('this.gameboard2 ', this.gameboard2 );
+        this.sqx[num] = 'sqx' + num;
+        console.log('this.sqx ', this.sqx);
+
+
+        if (this.didIWin('x')) {
+          console.log('I won');
+          this.gamewonPlayer = true;
+          this.gamewon = true;
+          this.reload = true;
+        } else {
+          //console.log('I did not win yet');
+          console.log('player 2 turn this.gameboard ', this.gameboard2);
+          this.player = 2;
+          this.randomSquare();
+        }
+      }
+
     }
-    //this.gameboard.splice(num, 0, 'sq' + num.toString() + 'x')
-    //console.log('this.gameboard after', this.gameboard);
+
   }
 
+
+
   didIWin(symbol) {
-    if ( _.includes(this.gameboard[0], symbol) && _.includes(this.gameboard[1], symbol) && _.includes(this.gameboard[2], symbol)) {
-      console.log('sq1, sq2 and sq3 all have a symbol');
+    if ( _.includes(this.gameboard2[0], symbol) && _.includes(this.gameboard2[1], symbol) && _.includes(this.gameboard2[2], symbol)) {
+      //console.log('sq1, sq2 and sq3 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[3], symbol) && _.includes(this.gameboard[4], symbol) && _.includes(this.gameboard[5], symbol)){
-      console.log('sq4, sq5 and sq6 all have a symbol');
+    } else if ( _.includes(this.gameboard2[3], symbol) && _.includes(this.gameboard2[4], symbol) && _.includes(this.gameboard2[5], symbol)){
+      //console.log('sq4, sq5 and sq6 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[6], symbol) && _.includes(this.gameboard[7], symbol) && _.includes(this.gameboard[8], symbol)  ) {
-      console.log('sq7, sq8 and sq9 all have a symbol');
+    } else if ( _.includes(this.gameboard2[6], symbol) && _.includes(this.gameboard2[7], symbol) && _.includes(this.gameboard2[8], symbol)  ) {
+      //console.log('sq7, sq8 and sq9 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[0], symbol) && _.includes(this.gameboard[3], symbol) && _.includes(this.gameboard[6], symbol)  )   {
-      console.log('sq1, sq4 and sq7 all have a symbol');
+    } else if ( _.includes(this.gameboard2[0], symbol) && _.includes(this.gameboard2[3], symbol) && _.includes(this.gameboard2[6], symbol)  )   {
+      //console.log('sq1, sq4 and sq7 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[1], symbol) && _.includes(this.gameboard[4], symbol) && _.includes(this.gameboard[7], symbol) ) {
-      console.log('sq2, sq5 and sq8 all have a symbol');
+    } else if ( _.includes(this.gameboard2[1], symbol) && _.includes(this.gameboard2[4], symbol) && _.includes(this.gameboard2[7], symbol) ) {
+      //console.log('sq2, sq5 and sq8 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[2], symbol) && _.includes(this.gameboard[5], symbol) && _.includes(this.gameboard[8], symbol) ) {
-      console.log('sq3, sq6 and sq9 all have a symbol');
+    } else if ( _.includes(this.gameboard2[2], symbol) && _.includes(this.gameboard2[5], symbol) && _.includes(this.gameboard2[8], symbol) ) {
+      //console.log('sq3, sq6 and sq9 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[0], symbol) && _.includes(this.gameboard[4], symbol) && _.includes(this.gameboard[8], symbol) ) {
-      console.log('sq1, sq5 and sq9 all have a symbol');
+    } else if ( _.includes(this.gameboard2[0], symbol) && _.includes(this.gameboard2[4], symbol) && _.includes(this.gameboard2[8], symbol) ) {
+      //console.log('sq1, sq5 and sq9 all have a symbol');
       return true;
-    } else if ( _.includes(this.gameboard[2], symbol) && _.includes(this.gameboard[4], symbol) && _.includes(this.gameboard[6], symbol)  ) {
-      console.log('sq3, sq5 and sq7 all have a symbol');
+    } else if ( _.includes(this.gameboard2[2], symbol) && _.includes(this.gameboard2[4], symbol) && _.includes(this.gameboard2[6], symbol)  ) {
+      //console.log('sq3, sq5 and sq7 all have a symbol');
       return true;
     } else {
-      console.log('does not include symbol');
+      //console.log('does not include symbol');
       return false;
     }
 
   }
 
-  randomSquare(randomNum) {
-    console.log('rand ', randomNum);
-    while (_.includes(this.gameboard, 'x') || _.includes(this.gameboard, 'o')) {
-      // yes then do something
-      this.gameboard.splice(randomNum, 0, 'sq' + randomNum.toString() + '0')
-      console.log('this.gameboard after', this.gameboard);
+  randomSquare() {
+    let rand = Math.floor(Math.random() * 9) + 1;
+
+    for (let i = 0; i < this.gameboard2.length + 1; i++) {
+      if ( _.includes(this.gameboard2[i], "") ) {
+
+        this.gameboard2[rand] = 'o';
+        console.log('randomsquare this.gameboard2 ', this.gameboard2);
+        //this.gameboard[rand] = 'sq' + rand.toString() + 'o';
+        //console.log('this.gameboard ', this.gameboard);
+        this.sqo[rand] = 'sqo' + rand;
+        console.log('this.sqo ', this.sqo);
+
+      }
+      if (this.didIWin('o')) {
+        console.log('Computer won');
+        this.gamewonComputer = true;
+        this.gamewon = true;
+        this.reload = true;
+      } else {
+        //console.log('I did not win yet');
+        //console.log('player 2 turn this.gameboard ', this.gameboard2);
+        this.player = 1;
+      }
+
     }
-    //this.gameboard.splice(randomNum, 0, '.sq' + randomNum.toString())
-    //console.log('this.gameboard after', this.gameboard);
-    console.log('this.gameboard[0], [1] and [2]', this.gameboard[0], this.gameboard[1], this.gameboard[2]);
-    return this.gameboard;
+
 
   }
 
-}
+
+  }
